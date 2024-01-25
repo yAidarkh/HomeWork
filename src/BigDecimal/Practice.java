81a79043e98b7b2ebd2f6451393010f784a6b528
Yerbol Aidarkhan <erbola119@gmail.com>
2024-01-25 22:05:42 +0600

    BigDecimal

--- src/BigDecimal/Practice.java
+++ src/BigDecimal/Practice.java
@@ -1,2 +1,18 @@
-package BigDecimal;public class Practice {
+package BigDecimal;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigDecimal a = BigDecimal.valueOf(scanner.nextInt());
+        BigDecimal b = BigDecimal.valueOf(scanner.nextInt());
+
+        System.out.println("Сложение: "+ a.add(b));
+        System.out.println("Вычитание: "+ BigDecimal.valueOf(Integer.max(a.intValue(),b.intValue())).subtract(BigDecimal.valueOf(Integer.min(a.intValue(),b.intValue()))));
+        System.out.println("Умножение: "+ a.multiply(b));
+        System.out.println("Деление: "+ BigDecimal.valueOf(Integer.max(a.intValue(),b.intValue())).divide(BigDecimal.valueOf(Integer.min(a.intValue(),b.intValue()))));
+    }
 }

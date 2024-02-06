b292c3fec02841ca1be5f90ed1f2d98cacdfc452
yAidarkh <47291614+yAidarkh@users.noreply.github.com>
2024-02-06 12:54:42 +0600

    Create Main.java

---
+++ src/StringTask/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Valid valid = new Valid();
+        System.out.println(valid.validPassword("Erbol1"));
+        System.out.println(valid.validMail("erbola@gmail.com"));
+        String words = "Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье";
+        SomeUtil someUtil = new SomeUtil();
+        someUtil.countConsonantsChar(words);
+        String months = "ЯНВАРЬ-ФЕВРАЛЬ-МАРТ-АПРЕЛЬ-МАЙ-ИЮНЬ-ИЮЛЬ-АВГУСТ-СЕНТЯБРЬ-ОКТЯБРЬ-НОЯБРЬ-ДЕКАБРЬ";
+        someUtil.printListFromString(months);
+        String sentence = "Сегодня отличный день для программирования";
+        System.out.println(someUtil.countWordInString(sentence));
+        String text = "Java удивительный язык программирования";
+        System.out.println(someUtil.findLongestWord(text));
+        String text2 = "Программирование на Java";
+        someUtil.countAllChar(text2);
+        String phrase = "Пример строки для конвертации";
+        someUtil.sentenceToSnakeSentence(phrase);
+    }
+}

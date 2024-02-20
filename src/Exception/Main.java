e70f476b35f62e7db138650792f0fca9e982c553
Ali Kali <dankot92@gmail.com>
2024-02-20 21:29:51 +0600

    ad

--- src/Exception/Main.java
+++ src/Exception/Main.java
@@ -2,6 +2,12 @@ package Exception;
 
 public class Main {
     public static void main(String[] args) {
+        ValidPassword validPassword = new ValidPassword();
+        try {
+            validPassword.validPassword("Password");
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
---
+++ src/Exception/ValidPassword.java
@@ -0,0 +1,18 @@
+package Exception;
+
+public class ValidPassword {
+    public void validPassword(String password) throws RuntimeException {
+        if(password.length()>=8){
+            for (int i = 0; i<password.length();i++){
+                if(Character.isUpperCase(password.charAt(i))){
+                    for (int j = 0; j<password.length();j++){
+                        if(Character.isDigit(password.charAt(j))){
+                            System.out.println("Пароль подходит.");
+                        }
+                    }
+                }
+            }
+        }
+        throw new RuntimeException("Пароль не подходит.");
+    }
+}

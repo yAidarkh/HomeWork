18779e8b8fd3b89283114554e6f203e46bf4ab24
Yerbol Aidarkhan <erbola119@gmail.com>
2024-02-22 22:07:17 +0600

    das

--- src/Exception/IntegerParser.java
+++ src/Exception/IntegerParser.java
@@ -15,6 +15,43 @@ public class IntegerParser{
         Integer a = Integer.parseInt(s);
         return a;
     }
+    public double parseDouble(String s){
+        if (s.isBlank()) {
+            throw new EmptyStringException("Введена пустая строка.");
+        }else if(!isDoubleString(s)){
+            throw new StringNotNumberException("Вы ввели не целое число.");
+        }else if (isToSmallDouble(s)){
+            throw new StringIsTooSmallNumberException("Введённое число слишком маленькое.");
+        }else if (s.length()>Double.MAX_VALUE){
+            throw new StringIsTooBigNumberException("Введённое число слишком большое.");
+        }
+        Double a = Double.parseDouble(s);
+        return a;
+    }
+
+    static boolean isDoubleString(String s){
+        int pointCount=0;
+        if(!(Character.isDigit(s.charAt(0)))||s.charAt(0)=='-') {
+            return false;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i)=='.'){
+                pointCount+=1;
+            }else if(!Character.isDigit(s.charAt(i))){
+                return false;
+            }
+        }
+        if (pointCount>1){
+            return false;
+        }
+        return true;
+    }
+    static boolean isToSmallDouble(String s){
+        if(s.charAt(0)=='-'&&s.length()>Double.MAX_VALUE){
+            return true;
+        }
+        return false;
+    }
 
     static boolean isToSmallNumber(String s){
         if(s.charAt(0)=='-'&&s.length()>11){
--- src/Exception/Main.java
+++ src/Exception/Main.java
@@ -10,11 +10,18 @@ public class Main {
 //        }catch (RuntimeException e){
 //            System.out.println(e.getMessage());
 //        }
-        System.out.print("Введите целое число => ");
+//        System.out.print("Введите целое число => ");
+//        Scanner scanner = new Scanner(System.in);
+//        final String inputValue = scanner.next();
+//        IntegerParser integerParser = new IntegerParser();
+//        final int parsedValue = integerParser.parseInt(inputValue);
+//        System.out.println(parsedValue);
+
+        System.out.print("Введите double число => ");
         Scanner scanner = new Scanner(System.in);
         final String inputValue = scanner.next();
         IntegerParser integerParser = new IntegerParser();
-        final int parsedValue = integerParser.parseInt(inputValue);
+        final double parsedValue = integerParser.parseDouble(inputValue);
         System.out.println(parsedValue);
 
     }

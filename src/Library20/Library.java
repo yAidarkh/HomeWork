d4e7b732717103a1bff08e0d945f6f08f611d76a
Yerbol Aidarkhan <erbola119@gmail.com>
2024-02-22 21:41:26 +0600

    asd

--- src/Library20/Library.java
+++ src/Library20/Library.java
@@ -6,32 +6,35 @@ public class Library {
     public Library(Book[] books) {
         this.books = books;
     }
-    public void addBook(Book book){
-        if(book.getTitle().isBlank()||book.getQuantity()<=0||book.getReleaseYear()<=0||book.getAgeSec()<=0){
+
+    public void addBook(Book book) {
+        if (book.getTitle().isBlank() || book.getQuantity() <= 0 || book.getReleaseYear() <= 0 || book.getAgeSec() <= 0) {
             throw new RuntimeException("Некоторые поля книги некорректно заполнены");
         }
         for (int i = 0; i < books.length; i++) {
-            if(books[i]==null){
-                books[i]=book;
+            if (books[i] == null) {
+                books[i] = book;
                 return;
             }
         }
     }
-    public void giveBook(int bookIndex, Reader reader){
+
+    public void giveBook(int bookIndex, Reader reader) {
         Book book = books[bookIndex];
-        if(book.getQuantity()<=0){
+        if (book.getQuantity() <= 0) {
             throw new RuntimeException("Такой книги нету, закончились.");
-        }else if (book.getAgeSec()> reader.getReaderAge()){
+        } else if (book.getAgeSec() > reader.getReaderAge()) {
             throw new RuntimeException("Вы слишком молоды.");
-        }else if (reader.getBorrowedBook()!=null) {
+        } else if (reader.getBorrowedBook() != null) {
             throw new RuntimeException("У вас уже есть книга.");
         }
-        books[bookIndex].setQuantity(book.getQuantity()-1);
+        books[bookIndex].setQuantity(book.getQuantity() - 1);
         reader.setBorrowedBook(book);
     }
-    public int findBook(String title){
+
+    public int findBook(String title) {
         for (int i = 0; i < books.length; i++) {
-            if(books[i].getTitle().equals(title)){
+            if (books[i].getTitle().equals(title)) {
                 return i;
             }
         }
--- src/Library20/Main.java
+++ src/Library20/Main.java
@@ -3,14 +3,14 @@ package Library20;
 public class Main {
     public static void main(String[] args) {
         Library library = new Library(new Book[100]);
-        library.addBook(new Book("1",1,3,17));
-        library.addBook(new Book("2",3,1,71));
-        library.addBook(new Book("3",1,1,18));
-        library.addBook(new Book("4",1,1,30));
+        library.addBook(new Book("1", 1, 3, 17));
+        library.addBook(new Book("2", 3, 1, 71));
+        library.addBook(new Book("3", 1, 1, 18));
+        library.addBook(new Book("4", 1, 1, 30));
         Reader reader = new Reader(18);
-        library.giveBook(library.findBook("3"),reader);
+        library.giveBook(library.findBook("3"), reader);
         Reader reader1 = new Reader(72);
-        library.giveBook(library.findBook("3"),reader1);
+        library.giveBook(library.findBook("3"), reader1);
 
     }
 }

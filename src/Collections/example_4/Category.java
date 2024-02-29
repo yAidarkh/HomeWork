f02df9f4fb80801a6d1a9e018510eba629354f4e
Yerbol Aidarkhan <erbola119@gmail.com>
2024-02-29 21:29:55 +0600

    asd

---
+++ src/Collections/example_4/Category.java
@@ -0,0 +1,54 @@
+package Collections.example_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+    private List<Product> productList = new ArrayList<>();
+
+    public Category(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public Product findMaxPriceProduct() {
+        Product productWithMaxPrice = new Product("", 0);
+        for (Product product : productList) {
+            if (product.getPrice() > productWithMaxPrice.getPrice()) {
+                productWithMaxPrice = product;
+            }
+        }
+        return productWithMaxPrice;
+    }
+
+    public int sumPriceProducts() {
+        int sum = 0;
+        for (Product product : productList) {
+            sum += product.getPrice();
+        }
+        System.out.printf("Общая цена товаров: %d\n", sum);
+        return sum;
+    }
+
+    public double calculateAverage() {
+        if (productList.isEmpty()) {
+            return 0;
+        }
+
+        int sum = 0;
+        for (Product product : productList) {
+            sum += product.getPrice();
+        }
+        System.out.printf("Средняя цена товара: %s\n", sum / productList.size());
+        return (double) sum / productList.size();
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+
+}
--- src/Collections/example_4/Main.java
+++ src/Collections/example_4/Main.java
@@ -5,25 +5,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Product> products = new ArrayList<>();
-        products.add(new Product("Хлеб", 150));
-        products.add(new Product("Молоко", 500));
-        products.add(new Product("Курица", 1000));
-        products.add(new Product("Говядина", 1500));
-
-        int sumProductPrice = 0;
-        int maxProductPrice = 0;
-        String maxProductPriceName = "";
-        for (Product product : products) {
-            System.out.println(product);
-            if (product.getPrice() > maxProductPrice) {
-                maxProductPrice = product.getPrice();
-                maxProductPriceName = product.getName();
-            }
-            sumProductPrice += product.getPrice();
-        }
-        System.out.printf("Средняя цена на товары %s\n", sumProductPrice / products.size());
-        System.out.printf("Максимальная цена товара %d, %s\n", maxProductPrice, maxProductPriceName);
-
+        List<Product> foodList = new ArrayList<>();
+        List<Product> phoneList = new ArrayList<>();
+        Category foods = new Category(foodList);
+        Category phones = new Category(phoneList);
+        foodList.add(new Product("Хлеб", 150));
+        foodList.add(new Product("Молоко", 500));
+        foodList.add(new Product("Курица", 1000));
+        foodList.add(new Product("Говядина", 1500));
+        phoneList.add(new Product("Xiaomi", 100_000));
+        phoneList.add(new Product("Samsung", 150_000));
+        phoneList.add(new Product("Iphone", 200_000));
+        phoneList.add(new Product("Nokia", 15_000));
+        System.out.println(foods.findMaxPriceProduct());
+        foods.sumPriceProducts();
+        foods.calculateAverage();
+        System.out.println(phones.findMaxPriceProduct());
+        phones.sumPriceProducts();
+        phones.calculateAverage();
     }
 }
